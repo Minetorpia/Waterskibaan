58e050a683e9fd425c1c8e12ecaf0ee5bf8cd796
Unknown <rminetorpia@gmail.com>
2019-10-03 22:01:45 +0200

    opdracht 1

---
+++ Waterskibaan/Sporter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    abstract class Sporter
+    {
+        public int AantalRondenNogTeGaan { get; set; }
+        public Zwemvest MyProperty { get; set; }
+        public Color KledingKleur { get; set; }
+        public List<IMoves> Moves { get; set; }
+
+        public Sporter(List<IMoves> moves)
+        {
+            Moves = moves;
+        }
+    }
+}
